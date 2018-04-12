a4b693d63928e78cf44cda9fcdf77cd33df18fe4
Richard Hedges <richardkhedges@sky.com>
2018-04-12 09:36:38 +0100

    Initial commit
    Added README
    
    Added plugin files:
    better-select.css, better-select.js
    
    Added example index.html

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# Better-Select
-Provides a better select with added features, including live search and multiple levels of optgroup
+# Better-Select `(jQuery)`
+
+Provides a better select with added features, including live search and multiple levels of optgroup.
+
+#### Usage Example
+
+```
+$('select').betterSelect({
+	inputClass: 'form-control',
+    selectedOption: function(option) {
+    	
+        alert('You have selected ' + $(option).text());
+        
+    }
+});
+```
+
+
+#### Paramaters
+
+```
+var args = {
+	inputClass: '',								// Specify the classes for the input search field
+    inputPlaceholder: 'Type here...',			// Specify the placeholder for the input search field
+    listMaxHeight: 0,							// Set a maximum height for the results list
+    											// Default is next positioned parents height
+    listSpace: 10,								// Offsets the results list from the search field
+    inline: false,								// When true, the results list will appear inline with content
+    selectedOption: function() {}				// A callback used to trigger events when an option is selected
+};
+```
+
+
+<br>
+##### Notice
+
+This plugin was created with two single projects in mind that required the use of multiple optgroups in multiple levels of a single select. The plugin successfully accomplishes it's intended purpose however does not yet update the selected items in the original text field. This will be added to the next update, however in the mean time you'll be required to collate the values of each `li` element with a class of `selected` as a child of `.better-select .options-list`
+
+##### Copyright
+Copyright 2018 Richard Hedges
+
+Licensed under the [MIT License](https://github.com/richardhedges/Better-Select/blob/master/LICENSE)
---
+++ better-select.js
@@ -0,0 +1,187 @@
+/**
+ * Better Select (Copyright 2018 Richard Hedges)
+ * License: MIT License (https://github.com/richardhedges/Better-Select/blob/master/LICENSE)
+ * 
+ * Provides a better select with added features,
+ * including live search and multiple levels of optgroup
+ * 
+ * @constructor
+ *
+ * @param {string} inputClass [Default: ""]
+ *   Specify the classes for the input search field
+ *
+ * @param {string} inputPlaceholder [Default: "Type here..."]
+ *   Specify the placeholder for the input search field
+ *
+ * @param {integer} listMaxHeight [Default: 0]
+ *   Set a maximum height for the results list
+ *   For dynamic height, set to 0
+ *
+ * @param {integer} listSpace [Default: 10]
+ *   Offsets the results list from the search field
+ *
+ * @param {boolean} inline [Default: false]
+ *   When set to true the results list will appear inline with content
+ *
+ * @param {callback} selectedOption
+ *   Specify a callback method to fire when an option is selected
+ *   Method args:
+ *      option - The options jQuery object
+ * 
+ */
+(function($) {
+  
+  $.fn.betterSelect = function(options) {
+    
+    var settings = $.extend({
+      inputClass: '',
+      inputPlaceholder: 'Type here...',
+      listMaxHeight: 0,
+      listSpace: 10,
+      inline: false,
+      selectedOption: function() { }
+    }, options);
+    
+    var betterSelects = [];
+    
+    $(this).each(function() {
+      
+      var betterSelectID = betterSelects.length + 1;
+      
+      betterSelects[betterSelectID] = {};
+      
+      $(this).wrap('<div id="better-select-' + betterSelectID + '" class="better-select" />');
+      var select = $(this).parent('.better-select');
+      
+      $(select).append('<input class="search-field ' + settings.inputClass + '" type="text" placeholder="' + settings.inputPlaceholder + '">');
+      
+      var maxHeight = $(select).offsetParent().height();
+      
+      if (settings.listMaxHeight > 0) {
+        maxHeight = settings.listMaxHeight;
+      } else {
+        maxHeight -= ($(select).height() + settings.listSpace);
+      }
+      
+      var listStyles = [
+        { key: 'max-height', val: maxHeight + 'px' },
+        { key: 'margin-top', val: settings.listSpace + 'px' }
+      ].map(function(style) {
+        return style.key + ':' + style.val;
+      }).join(';');
+      
+      var listClasses = '';
+     
+      if (settings.inline) {
+        listClasses = 'inline';
+      }
+      
+      $(select).append('<ul class="options-list ' + listClasses + '" style="' + listStyles + '"></ul>');
+      var optionsList = $('.options-list', select);
+      
+      var option, currentLevel, topLevel, tempLevel = null;
+      
+      $('> optgroup', this).each(function() {
+        
+        tempLevel = $('<li class="title"><strong>' + $(this).attr('label') + '</strong><ul></ul></li>');
+        
+        if (typeof $(this).attr('data-level') != 'undefined' && $(this).attr('data-level') == 'top') {
+          $(optionsList).append(tempLevel);
+          topLevel = $('> ul', tempLevel);
+        } else {
+          $(topLevel).append(tempLevel);
+        }
+        
+        currentLevel = $('> ul', tempLevel);
+        
+        $('> option', this).each(function() {
+          
+          var dataAtts = '';
+          
+          if (typeof $(this).attr('value') != 'undefined') {
+            dataAtts += ' data-value="' + $(this).attr('value') + '"';
+          }
+          
+          $.each($(this).data(), function(index, value) {
+            dataAtts += ' data-' + index + '="' + value + '"';
+          });
+          
+          option = $('<li' + dataAtts + '>' + $(this).text() + '</li>');
+          currentLevel.append(option);
+          
+        });
+        
+        
+      });
+      
+    });
+    
+    $('body').on('keyup', '.better-select .search-field', function() {
+      
+      var betterSelect = $(this).parents('.better-select');
+      var searchTerm = $(this).val().trim();
+      
+      openBetterSelect(betterSelect);
+      
+      if (searchTerm === '') {
+        
+        $(this).val('');
+        $('.options-list li:not(.title)', betterSelect).show();
+        
+      } else {
+        
+        $('.options-list li:not(.title)', betterSelect).each(function() {
+
+          if ($(this).text().toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+            $(this).show();
+          } else {
+            $(this).hide();
+          }
+
+        });
+        
+      }
+      
+    });
+    
+    $('body').on('click', '.better-select .options-list li:not(.title)', function() {
+      
+      $(this).toggleClass('selected');
+      
+      settings.selectedOption(this);
+      
+    });
+    
+    $('body').on('focus', '.better-select .search-field', function() {      
+      openBetterSelect($(this).parents('.better-select'));
+    });
+    
+    $(document).on('click', function(event) {
+      
+      if (!$(event.target).parents('.better-select.active').length && $('.better-select.active').length) {
+        closeBetterSelect($('.better-select.active'));
+      }
+      
+    });
+    
+    function openBetterSelect(select) {
+      
+      if ($(select).length) {
+        $('.options-list', select).show();
+        $(select).addClass('active');
+      }
+      
+    }
+    
+    function closeBetterSelect(select) {
+      
+      if ($(select).length) {
+        $('.options-list', select).hide();
+        $(select).removeClass('active');
+      }
+      
+    }
+    
+  };
+  
+}(jQuery));
